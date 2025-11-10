fbf096ca01717f38bd426827e665439308f9573b
Lindeberg Sabreu - <128699482+LindebergSabreu@users.noreply.github.com>
2025-11-10 14:06:44 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css';
+import { ReactNode } from 'react';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
